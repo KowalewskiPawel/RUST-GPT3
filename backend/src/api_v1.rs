c01066875076f6a9a1be9c15fbc008147185f3de
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-01-13 08:36:16 +0100

    add ip address

--- backend/src/api_v1.rs
+++ backend/src/api_v1.rs
@@ -1,5 +1,6 @@
+use std::net::SocketAddr;
 
 #[get("/v1/test/<text>")]
-pub fn test(text: String) -> String {
-    format!("This is test: {}", text)
+pub fn test(remote_addr: SocketAddr, text: String) -> String {
+    format!("This is your text: {:?} and text: {}", remote_addr, text)
 }
