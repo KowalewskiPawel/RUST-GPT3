39850e211e8835f7f0f50ce72fcb2bb60e1555b5
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-01-13 13:23:41 +0100

    add sqlite

--- backend/src/api_v1.rs
+++ backend/src/api_v1.rs
@@ -1,8 +1,11 @@
+use crate::appconfig;
+use sqlite::State;
 use reqwest;
 use reqwest::header::*;
 use rocket::serde::json::Json;
 use rocket::serde::{Deserialize, Serialize};
 use std::env;
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
@@ -46,6 +49,30 @@ pub struct GptResponse {
     choices: Vec<GptChoices>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RequestId {
+    id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AddKey {
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Keys {
+    key: String,
+    tokens_left: String
+}
+impl Default for Keys {
+    fn default() -> Keys {
+        Keys {
+            key: String::new(),
+            tokens_left: String::new()
+        }
+    }
+}
+
 #[get("/v1/test/<text>")]
 pub fn test(text: String) -> String {
     format!("This is your text: {}", text)
@@ -73,4 +100,69 @@ pub async fn send_rq(gpt_prompt: Json<GptPrompt>) -> String {
         Ok(text) => format!("{:?}", text.choices[0].text),
         Err(error) => format!("{:?}", error),
     }
+}
+
+#[post("/v1/add_key", format = "json", data = "<add_key>")]
+pub fn create_key(add_key: Json<AddKey>) -> String {
+    let conn = sqlite::open(appconfig::DATABASE_FILE).expect("Database not readable!");
+    
+    let admin_key = add_key.password.to_owned();
+    let api_key: String = env::var("ADMIN_KEY").unwrap();
+
+    if admin_key != api_key {
+        return "Wrong admin key!".to_string();
+    }
+    let id = Uuid::new_v4();
+
+    let result: String = "SUCCESS".to_string();
+    let _statement = match conn.execute(format!(
+        "INSERT INTO keys values ('{}', '{}')", id, 10 )) {
+        Ok(statement) => statement,
+        Err(e) => return format!("Problem running query: {:?}", e),
+    };
+
+    result
+}
+
+#[get("/v1/query_all", format = "json", data = "<admin_key>")]
+pub fn query_all(admin_key: Json<AddKey>) -> Json<Vec<Keys>> {
+    appconfig::check_dbfile(appconfig::DATABASE_FILE);
+
+    let conn = sqlite::open(appconfig::DATABASE_FILE).expect("Database not readable!"); //we can unwrap we checked the file exists
+    let admin_key = admin_key.password.to_owned();
+    let api_key: String = env::var("ADMIN_KEY").unwrap();
+    let mut result: Vec<Keys> = Vec::new();
+
+    if admin_key != api_key {
+        result.push(Keys { key: "Wrong Admin key!".to_string(), tokens_left: "0".to_string() });
+        return Json(result);
+    }
+
+    
+
+    let query = "SELECT * FROM keys";
+
+    let mut keys_new = Keys::default();
+
+    conn.iterate(query, |pairs| {
+        for &(name, value) in pairs.iter() {
+            let current_value = value.unwrap();
+            match name {
+                "id" => keys_new.key = String::from(current_value),
+                "left" => {
+                    keys_new.tokens_left = String::from(current_value);
+                    let keys_copy = Keys {
+                        key: keys_new.key.clone(),
+                        tokens_left: keys_new.tokens_left.clone(),
+                    };
+                    result.push(keys_copy);
+                }
+                &_ => (),
+            }
+        }
+        true
+    })
+    .unwrap();
+
+    Json(result)
 }
