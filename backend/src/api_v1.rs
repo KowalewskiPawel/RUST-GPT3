564c19855112958cb23cb3b4bbc0e019fe1ad913
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-01-13 09:39:50 +0100

    remove ip

--- backend/src/api_v1.rs
+++ backend/src/api_v1.rs
@@ -1,6 +1,5 @@
-use std::net::SocketAddr;
 
 #[get("/v1/test/<text>")]
-pub fn test(remote_addr: SocketAddr, text: String) -> String {
-    format!("This is your text: {:?} and text: {}", remote_addr, text)
+pub fn test(text: String) -> String {
+    format!("This is your text: {}", text)
 }
